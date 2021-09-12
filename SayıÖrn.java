53952ed1d1563435cc2070ddd881bb2b67913c9e
s-jx <88933003+gonichiva@users.noreply.github.com>
2021-09-12 22:40:11 +0300

    Update SayıÖrn.java

--- SayıÖrn.java
+++ SayıÖrn.java
@@ -1,5 +1,6 @@
 class Main {
-  public static void main(String[] args){  System.out.println("Sayı Atama");
+  public static void main(String[] args){ 
+    System.out.println("Sayı Atama");
        int sayı1=10;
        int sayı2=20;
        sayı1 = sayı2;
@@ -8,8 +9,8 @@ class Main {
  
        int[] sayılar1 = {1,2,3,4,5};
        int[] sayılar2 = {10,20,30,40,50};
-        sayılar1 = sayılar2;
-        sayılar2[0]= 100;
+       sayılar1 = sayılar2;
+       sayılar2[0]= 100;
        System.out.println(sayılar1[0]);
       
    } 
