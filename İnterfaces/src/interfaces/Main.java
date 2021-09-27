79e8a4d58a86f70b489226a4d293ef6ae5d5cb1d
Bahar <88933003+gonichiva@users.noreply.github.com>
2021-09-27 15:41:07 +0300

    Update Main.java

--- İnterfaces/src/interfaces/Main.java
+++ İnterfaces/src/interfaces/Main.java
@@ -1,11 +1,17 @@
 package interfaces;
 
 public class Main {
-    private Logger loggler;
+   //CustomerManager customerManager = new CustomerManager(new SmsLogger());
 
     public void main(String[] args) {
-       CustomerManager customermanager = new CustomerManager(loggler);
+      //CustomerManager customerManager = new CustomerManager(new SmsLogger());
+       // Multi Logger
+		Logger[] loggers = {new SmsLogger(), new EmailLogger()};
+		
+        CustomerManager customerManager2 = new CustomerManager(loggers);
+        CustomerManager customermanager = new CustomerManager(loggler);
        Customer Cansu = new Customer(1, "Cansu", "Aydın");
-       customermanager.add(Cansu);
+    
+        customermanager.add(Cansu);
    }
 }
